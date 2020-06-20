6df2cf21cd207b82e6b69a700c8c460e24e446c2
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-06-20 15:05:24 +1000

    Saving progress of adding professional work to portfolio.

--- src/Components/common/ProjectViewer.js
+++ src/Components/common/ProjectViewer.js
@@ -8,12 +8,6 @@ import css from "./ProjectViewer.module.css";
 const ProjectViewer = ({ title, open, toggleModal, achievements }) => {
   const style = {
     content: {
-      // top                   : '50%',
-      // left                  : '50%',
-      // right                 : 'auto',
-      // bottom                : 'auto',
-      // marginRight           : '-50%',
-      // transform             : 'translate(-50%, -50%)'
       padding: "50px",
     },
   };
