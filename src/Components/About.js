40551c51ffce91b857f4e83914d5ca2c08418514
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-06-22 09:34:56 +1000

    Changed alt of profile pic image to include my name instead of original author's.

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,7 +22,7 @@ class About extends Component {
             <img
               className="profile-pic"
               src={profilepic}
-              alt="Tim Baker Profile Pic"
+              alt="Anthony Hien Vu's Profile Pic"
             />
           </div>
           <div className="nine columns main-col">
