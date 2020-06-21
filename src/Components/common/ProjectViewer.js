895d3dcbe05a0329bf9a30681346387d858f778a
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-06-21 15:42:16 +1000

    Removed redundant css import statement.

--- src/Components/common/ProjectViewer.js
+++ src/Components/common/ProjectViewer.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
-// import "./ProjectViewer.css";
 
 const ProjectViewer = ({ title, open, toggleModal, achievements }) => {
   const style = {
