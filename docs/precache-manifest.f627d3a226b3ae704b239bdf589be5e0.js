6551dfff414f7d99b76177f3c04d961aa70e0d7d
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-06-23 09:14:04 +1000

    Created build files and stored in /docs folder for gh-pages deployment.

---
+++ docs/precache-manifest.f627d3a226b3ae704b239bdf589be5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef63857d57c594a2f11e1d5b7da12a4",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "70ca551143a1b8becb3f",
+    "url": "/portfolio-website/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "70ca551143a1b8becb3f",
+    "url": "/portfolio-website/static/js/2.7ed03c11.chunk.js"
+  },
+  {
+    "revision": "74dafa63aa41d7e1ca3cdf65942af549",
+    "url": "/portfolio-website/static/js/2.7ed03c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb957d4d1b913b730daf",
+    "url": "/portfolio-website/static/js/main.5866253f.chunk.js"
+  },
+  {
+    "revision": "99cb685b86efd0f580da",
+    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+  }
+]);
