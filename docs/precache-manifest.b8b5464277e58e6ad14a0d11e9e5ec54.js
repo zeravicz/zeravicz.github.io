6d1d76c59b46c40e4914a85b67a78bc34a385db2
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-10-14 17:35:45 +1100

    Added docs folder containing build files for my gh-pages website.

---
+++ docs/precache-manifest.b8b5464277e58e6ad14a0d11e9e5ec54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6286e32745a1da671e6060149c44ae1f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aaac3e8216936ff5b72d",
+    "url": "/portfolio/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "aaac3e8216936ff5b72d",
+    "url": "/portfolio/static/js/2.30bd868f.chunk.js"
+  },
+  {
+    "revision": "74dafa63aa41d7e1ca3cdf65942af549",
+    "url": "/portfolio/static/js/2.30bd868f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea501539ad68cc061739",
+    "url": "/portfolio/static/js/main.5fcfd46b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
